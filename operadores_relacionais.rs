cb8eb5161040e184770bd62ce00f22c6f6547a0c
Dev-student888 <124001676+Dev-student888@users.noreply.github.com>
2024-05-27 13:55:19 -0300

    Add files via upload

---
+++ operadores_relacionais.rs
@@ -0,0 +1,20 @@
+/* OS OPERADORES RELACIONAIS SÃO:
+   - MENOR QUE -> `<` 
+   - MAIOR QUE -> `>` 
+   - MENOR OU IGUAL A -> `<=` 
+   - MAIOR OU IGUAL A -> `>=` 
+   - DIFERENTE DE -> `!=`
+   - IGUAL A -> `==`
+   
+   OBS.: DESCONSIDERAR O SINAL DE ASPAS.
+*/
+
+fn main(){
+
+    let maior = 5;
+    let menor = 3;
+
+    if maior > menor{
+        println!("{} é maior que {}. ", maior, menor);
+    }
+}
