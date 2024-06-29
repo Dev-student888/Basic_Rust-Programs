ef8954df5d2aee9660c7eb3fc63f14b56b6d6eea
Ian Ribeiro Lacerda <124001676+Dev-student888@users.noreply.github.com>
2024-06-29 13:58:37 -0300

    Delete ferris_says directory

--- ferris_says/target/debug/deps/ferris_says-0941ffa016fb5b74.d
+++
@@ -1,5 +0,0 @@
-C:\Users\ianan\Documents\Programação e Afins\Rust_Projects\ferris_says\target\debug\deps\libferris_says-0941ffa016fb5b74.rmeta: src/main.rs
-
-C:\Users\ianan\Documents\Programação e Afins\Rust_Projects\ferris_says\target\debug\deps\ferris_says-0941ffa016fb5b74.d: src/main.rs
-
-src/main.rs:
--- ferris_says/target/debug/deps/ferris_says-284f5702176d608a.d
+++
@@ -1,5 +0,0 @@
-C:\Users\ianan\Documents\Programação e Afins\Rust_Projects\ferris_says\target\debug\deps\libferris_says-284f5702176d608a.rmeta: C:\Users\ianan\.cargo\registry\src\index.crates.io-6f17d22bba15001f\ferris-says-0.3.1\src\lib.rs
-
-C:\Users\ianan\Documents\Programação e Afins\Rust_Projects\ferris_says\target\debug\deps\ferris_says-284f5702176d608a.d: C:\Users\ianan\.cargo\registry\src\index.crates.io-6f17d22bba15001f\ferris-says-0.3.1\src\lib.rs
-
-C:\Users\ianan\.cargo\registry\src\index.crates.io-6f17d22bba15001f\ferris-says-0.3.1\src\lib.rs:
