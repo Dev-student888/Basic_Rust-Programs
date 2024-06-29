f22ad9cc6b20d3fc831fc85587d6109be455f461
Ian Ribeiro Lacerda <124001676+Dev-student888@users.noreply.github.com>
2024-06-29 20:41:23 -0300

    Add files via upload

--- match_case.rs
+++ match_case.rs
@@ -1,7 +1,17 @@
+/* USAMOS O `match` CASE PARA ENCONTRAR UM VALOR CORRESPONDENTE A UM TIPO DE DADO
+   A SINTAXE DA ESTRUTURA `match` CASE É A SEGUINTE:
+        match nome_da_variavel { nome_da_variavel REFERE-SE AO PRÓPRIO NOME DA VARIÁVEL ESCOLHIDA
+            "opção01" => RESPOSTA PARA A OPÇÃO 1; EX: println!() DEPOIS VEM A VÍRGULA `,` SEM ASPAS
+            "opção02" => RESPOSTA PARA A OPÇÃO 2,
+            "..." => PODE COLOCAR QUANTAS OPÇÕES QUISER,
+            _ => RESPOSTA PARA A OPÇÃO `default` DO `match` CASE
+        } 
+*/
+
 fn main(){
     let fruta: &str = "manga";
 
-    match fruta { // Usamos o `match` case para encontrar um valor correspondente a um tipo de dado
+    match fruta { 
         "manga" => println!("Eu tenho uma {fruta} nas mãos"), // Nesta opção colocamos "manga" como alternativa
         "banana" => println!("Eu tenho uma {fruta} nas mãos"), // Nesta opção colocamos "banana" como alternativa
         "pêra" => println!("Eu tenho uma {fruta} nas mãos"), // ...
